5d291465134e30077e1bb3f59ffda7d9ce6cf79a
pvvx <pvvx@mail.ru>
2015-09-28 08:55:33 +0300

    add samples webfs py

---
+++ webfs/webfs_upload2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: cp1251 -*-
+'''
+test webfs_upload.py
+Created on 27/09/2015.
+
+@author: PVV
+'''
+#
+# Use module 'requests' - http://docs.python-requests.org/
+#
+ 
+import requests 
+from requests.auth import HTTPBasicAuth
+ 
+ 
+if __name__ == '__main__':
+    espurl = 'http://192.168.4.1/fsupload'
+    username = 'ESP8266'
+    password = '0123456789'
+    filename = '../WEBFiles.bin'
+
+    files = {'file': (filename, open(filename, 'rb'), 'application/octet-stream')}
+#    postdata = {'':''}
+#    headers = {'Connection' : 'keep-alive'}
+    
+    s = requests.Session()
+#    q = s.post(theurl, auth=HTTPBasicAuth(username, password), headers=headers, data=postdata, files=files)
+    q = s.post(espurl, auth=HTTPBasicAuth(username, password), files=files)
+    print(q.status_code)
