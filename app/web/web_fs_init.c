a80316bfbdbb52399152a1d77c26d0c8efd8e18b
pvvx <pvvx@mail.ru>
2015-11-18 00:42:15 +0300

    update

--- app/web/web_fs_init.c
+++ app/web/web_fs_init.c
@@ -23,10 +23,21 @@ struct buf_fini
 {
 	TCP_SERV_CONN ts_conn;
 	WEB_SRV_CONN web_conn;
-	uint8 buf[FINI_BUF_SIZE];
+	uint8 buf[FINI_BUF_SIZE+1];
 };
 
-void web_fini(const uint8 * fname)
+/******************************************************************************
+*******************************************************************************/
+LOCAL uint16 ICACHE_FLASH_ATTR find_crlf(uint8 * chrbuf, uint16 len) {
+  int i;
+  for(i = 0; i < len; i++) {
+	  if(chrbuf[i] == '\n' || chrbuf[i] == '\r' || chrbuf[i] == '\0')  return i;
+  }
+  return len;
+}
+/******************************************************************************
+*******************************************************************************/
+void ICACHE_FLASH_ATTR web_fini(const uint8 * fname)
 {
 	struct buf_fini *p = (struct buf_fini *) os_zalloc(sizeof(struct buf_fini));
 	if(p == NULL) {
@@ -75,36 +86,38 @@ void web_fini(const uint8 * fname)
 		}
 		uint16 len = WEBFSGetArray(web_conn->webfile, pstr, FINI_BUF_SIZE);
 #if DEBUGSOO > 3
-		os_printf("ReadF[%u]=%u\n",web_conn->webfile, len);
+		os_printf("ReadF[%u]=%u\n", web_conn->webfile, len);
 #endif
 		if(len) { // есть байты в файле
-			uint8 *pend = web_strnstr(pstr, CRLF, FINI_BUF_SIZE);
-			if(pend != NULL) {
-				int cmp = pend - pstr;
-				if(cmp >= 0) { // найден CRLF
-					// откат файла
-					WEBFSStubs[web_conn->webfile].addr -= len;
-					WEBFSStubs[web_conn->webfile].bytesRem += len;
-					// передвинуть указатель в файле на считанные байты с учетом маркера, без добавки длины для передачи
-					WEBFSStubs[web_conn->webfile].addr += cmp+2;
-					WEBFSStubs[web_conn->webfile].bytesRem -= cmp+2;
-					if(cmp != 0) {
-						pstr[cmp] = '\0'; // закрыть string calback-а
+			pstr[len] = '\0';
+			int sslen = 0;
+			while(pstr[sslen] == '\n' || pstr[sslen] == '\r') sslen++;
+			if(sslen == 0) {
+				int nslen = find_crlf(pstr, len);
+				if(nslen != 0) {
+					pstr[nslen] = '\0'; // закрыть string calback-а
+					while((nslen < len) && (pstr[nslen] == '\n' || pstr[nslen] == '\r')) nslen++;
 #if DEBUGSOO > 3
-						os_printf("String:%s\n", pstr);
+					os_printf("String:%s\n", pstr);
 #endif
 
-						if(!os_memcmp((void*)pstr, "inc:", 4)) { // "inc:file_name"
-							if(!web_inc_fopen(ts_conn, &pstr[4])) {
+					if(!os_memcmp((void*)pstr, "inc:", 4)) { // "inc:file_name"
+						if(!web_inc_fopen(ts_conn, &pstr[4])) {
 #if DEBUGSOO > 1
-								os_printf("file not found!");
+							os_printf("file not found!");
 #endif
-							};
-						}
-						else web_int_callback(ts_conn);
-					};
+						};
+					}
+					else web_int_callback(ts_conn);
 				};
+				sslen = nslen;
 			};
+			// откат файла
+			WEBFSStubs[web_conn->webfile].addr -= len;
+			WEBFSStubs[web_conn->webfile].bytesRem += len;
+			// передвинуть указатель в файле на считанные байты с учетом маркера, без добавки длины для передачи
+			WEBFSStubs[web_conn->webfile].addr += sslen;
+			WEBFSStubs[web_conn->webfile].bytesRem -= sslen;
 		}
 		else if(web_inc_fclose(web_conn)) {
 			if(web_conn->web_disc_cb != NULL) web_conn->web_disc_cb(web_conn->web_disc_par);
