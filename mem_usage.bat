805c2aad3dacc842cdc1f7d40ac62d9d560c59a7
pvvx <pvvx@mail.ru>
2016-09-03 07:15:59 +0300

    update

--- mem_usage.bat
+++ mem_usage.bat
@@ -1,1 +1,1 @@
-C:\Espressif\utils\MemAnalyzer.exe C:\Espressif\xtensa-lx106-elf\bin\xtensa-lx106-elf-objdump.exe .\app\.output\eagle\image\eagle.app.v6.out >>mem.txt
+C:\Espressif\utils\ESP8266\MemAnalyzer.exe C:\Espressif\xtensa-lx106-elf\bin\xtensa-lx106-elf-objdump.exe .\app\.output\eagle\image\eagle.app.v6.out >>mem.txt
